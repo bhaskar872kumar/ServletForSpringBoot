2f5dcfe86ec1fbfca25afa7af8e9867d51c28c2a
Bhaskar872kumar <kumarbhaskar872@gmail.com>
2025-03-11 15:32:25 +0530

    HttpSession interface

--- DemoServlet/src/main/java/com/cu/AddServlet.java
+++ DemoServlet/src/main/java/com/cu/AddServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class AddServlet extends HttpServlet
 {
@@ -16,16 +17,10 @@ public class AddServlet extends HttpServlet
 		int j=Integer.parseInt(req.getParameter("num2"));
 		int k=i+j;
 		
-//		res.sendRedirect("sq?k="+k);
-		res.sendRedirect("sq");
-//		If we don't send the value of k here the the second Servlet would 
-//		get an error while trying to fetch the value of k
-//		and would redirect us to
-//		http://localhost:8081/DemoServlet/sq
-//		either in the browser we can add ?k=9 at the end 
-//		so the url becomes http://localhost:8081/DemoServlet/sq?k=9 -> SecondServlet would run
-
+		HttpSession session = req.getSession();
 		
+		session.setAttribute("k",k);
+		res.sendRedirect("sq");
 		
 
 		
--- DemoServlet/src/main/java/com/cu/SqServlet.java
+++ DemoServlet/src/main/java/com/cu/SqServlet.java
@@ -6,12 +6,14 @@ import java.io.PrintWriter;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class SqServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req,HttpServletResponse res) throws IOException {
+		
+		HttpSession session=req.getSession();
 			
-		int k=Integer.parseInt(req.getParameter("k"));
-//		here we are trying to get the value of k which has not been sent by firstServlet(addServlet)
+		int k=(int) session.getAttribute("k");
 		PrintWriter out=res.getWriter();
 		out.println("you are on the second servlet and the square is "+k*k);
 	}
